5238c13d9dd4859ebf42b11b6121e863ae50ccd5
sla <uouo33@naver.com>
2019-04-02 20:53:54 +0900

    동영상 fullscreen 전까지

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -46,7 +46,14 @@ function handleRangeUpdte() {
 }
 
 function handleProgress() {
-    
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`; 
+}
+
+function scrub(e) {
+    //console.log(e);
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
 }
 
 
@@ -54,7 +61,15 @@ function handleProgress() {
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip)); 
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdte));
 ranges.forEach(range => range.addEventListener('mousemove',handleRangeUpdte));
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup',() => mousedown = false);
